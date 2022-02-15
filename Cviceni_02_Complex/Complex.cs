efd5fba269a79912af28e7fa94d3eb3f7997fc7e
Student <Student@PC-SE5113-12>
2022-02-15 12:13:48 +0100

    t

--- Cviceni_02_Complex/Complex.cs
+++ Cviceni_02_Complex/Complex.cs
@@ -10,6 +10,7 @@ namespace Cviceni_02_Complex
     {
         double realna;
         double imaginarni;
+        double sranda = 0.0;
 
         public Complex(double real = 0.0, double complex = 0.0)
         {
